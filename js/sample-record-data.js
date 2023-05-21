620c233f3ed968cb3714e8e857bdad94d23d6a91
katerynarieznik <rieznik.k@gmail.com>
2023-05-21 19:37:01 +0300

    add sample record data

---
+++ js/sample-record-data.js
@@ -0,0 +1,76 @@
+const records = [
+  {
+    id: 'record-1',
+    title: 'Crush',
+    artist: 'Cigarettes After Sex',
+    year: '2018',
+    style: ['Indie Rock']
+    country: 'US',
+    coverImages: {
+      avif: ['img/covers/1x/cover1-1x.avif', 'img/covers/2x/cover1-2x.avif'],
+      jpg: ['img/covers/1x/cover1-1x.jpg', 'img/covers/2x/cover1-2x.jpg'],
+    },
+  },
+  {
+    id: 'record-2',
+    title: 'The Moon Is Down',
+    artist: 'Further Seems Forever',
+    year: '2022',
+    style: ['Emo', 'Indie Rock'],
+    country: 'US',
+    coverImages: {
+      avif: ['img/covers/1x/cover2-1x.avif', 'img/covers/2x/cover2-2x.avif'],
+      jpg: ['img/covers/1x/cover2-1x.jpg', 'img/covers/2x/cover2-2x.jpg'],
+    },
+  },
+  {
+    id: 'record-3',
+    title: 'Senbernar',
+    artist: 'Latexfauna',
+    year: '2023',
+    style: ['Indie Pop'],
+    country: 'Ukraine',
+    coverImages: {
+      avif: ['img/covers/1x/cover3-1x.avif', 'img/covers/2x/cover3-2x.avif'],
+      jpg: ['img/covers/1x/cover3-1x.jpg', 'img/covers/2x/cover3-2x.jpg'],
+    },
+  },
+  {
+    id: 'record-4',
+    title: 'How To Let Go',
+    artist: 'Sigrid',
+    year: '2022',
+    style: ['Indie Pop'],
+    country: 'UK',
+    coverImages: {
+      avif: ['img/covers/1x/cover4-1x.avif', 'img/covers/2x/cover4-2x.avif'],
+      jpg: ['img/covers/1x/cover4-1x.jpg', 'img/covers/2x/cover4-2x.jpg'],
+    },
+  },
+  {
+    id: 'record-5',
+    title: 'Live 2018',
+    artist: 'Stone Temple Pilots',
+    year: '2018',
+    style: ['Grunge'],
+    country: 'USA & Canada',
+    coverImages: {
+      avif: ['img/covers/1x/cover5-1x.avif', 'img/covers/2x/cover5-2x.avif'],
+      jpg: ['img/covers/1x/cover5-1x.jpg', 'img/covers/2x/cover5-2x.jpg'],
+    },
+  },
+  {
+    id: 'record-6',
+    title: 'Anniversary Trilogy Box Set',
+    artist: 'Bad Bunny',
+    year: '2021',
+    style: ['Reggaeton'],
+    country: 'USA',
+    coverImages: {
+      avif: ['img/covers/1x/cover6-1x.avif', 'img/covers/2x/cover6-2x.avif'],
+      jpg: ['img/covers/1x/cover6-1x.jpg', 'img/covers/2x/cover6-2x.jpg'],
+    },
+  },
+];
+
+export { records };
