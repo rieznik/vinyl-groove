b1d185d3fed83389a3db908cad6087bc20a2463e
katerynarieznik <rieznik.k@gmail.com>
2023-05-22 21:14:51 +0300

    add three dots pagination logic

--- js/renderPagination.js
+++ js/renderPagination.js
@@ -1,18 +1,47 @@
+const getPagination = (current, total, delta = 2, gap = '...') => {
+  if (total <= 1) return [1];
+
+  const center = [current];
+
+  for (let i = 1; i <= delta; ++i) {
+    center.unshift(current - i);
+    center.push(current + i);
+  }
+
+  const filteredCenter = center.filter((page) => page > 1 && page < total);
+
+  const includeLeftGap = current > 3 + delta;
+  const includeLeftPages = current === 3 + delta;
+  const includeRightGap = current < total - (2 + delta);
+  const includeRightPages = current === total - (2 + delta);
+
+  if (includeLeftPages) filteredCenter.unshift(2);
+  if (includeRightPages) filteredCenter.push(total - 1);
+  if (includeLeftGap) filteredCenter.unshift(gap);
+  if (includeRightGap) filteredCenter.push(gap);
+
+  return [1, ...filteredCenter, total];
+};
+
 const renderPagination = (totalRecords, recordsPerPage, currentPage) => {
   const paginationListElement = document.querySelector('.pagination');
   paginationListElement.innerHTML = '';
+
   const totalPages = Math.ceil(totalRecords / recordsPerPage);
 
-  for (let i = 1; i <= totalPages; i++) {
+  const pagination = getPagination(currentPage, totalPages);
+
+  for (const page of pagination) {
     const pageElement = document.createElement('li');
     pageElement.classList = 'pagination__page';
 
-    if (i === currentPage) {
+    if (page === currentPage) {
       pageElement.classList.add('pagination__page_active');
+    } else if (page === '...') {
+      pageElement.classList.add('pagination__page_more');
     }
 
-    pageElement.innerHTML = `<a href="index.html?page=${i}">${i}</a>`;
-
+    pageElement.innerHTML = `<a href="index.html?page=${page}">${page}</a>`;
     paginationListElement.appendChild(pageElement);
   }
 };
